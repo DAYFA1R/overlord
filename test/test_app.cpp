154032b33c296aa291d2f5bb35e94849083f100f
DAYFA1R <daniel@nup.onl>
2018-06-14 18:55:47 -0400

    Test: adding test for default cmd

--- test/test_app.cpp
+++ test/test_app.cpp
@@ -1,21 +1,41 @@
 #include "gtest/gtest.h"
+#include "yaml-cpp/yaml.h"
+
+// Test this source file
 #include "../src/app.cpp"
 
 namespace {
-  // Stub args
-  int argc = 2;
-  char* argv[] = {
-    "bin/ov"
-    "list",
-    "-r"
-  };
+  // Global stubs
+  // Stub config
+  YAML::Node node;
+  node["uid"] = "sample-project";
+  node["dir"] = "~/Projects/sandbox";
 
-  // Create overlord instance with stubbed args
-  App overlord(argc, argv);
 
   // Test if App::primaryCommand is the second value in argv
   TEST(AppConstructor, primaryCommandIsArgAfterExe) {
-    // See if both strings are equal
+    // Local stubs
+    int argc = 2;
+    char* argv[] = {
+      "bin/ov"
+      "list",
+      "-r"
+    };
+    
+    // Create overlord instance and assert
+    App overlord(argc, argv);
     ASSERT_STREQ(overlord.getPrimaryCommand().c_str(), argv[1]);
+  }
+
+  TEST(AppConstructor, primaryCommandDefaultsToHelp) {
+    // Local stubs
+    int argc = 1;
+    char* argv[] = {
+      "bin/ov"
+    };
+
+    // Create overlord instance and assert
+    App overlord(argc, argv);
+    ASSERT_STREQ(overlord.getPrimaryCommand().c_str(), "help");
   }
 }
