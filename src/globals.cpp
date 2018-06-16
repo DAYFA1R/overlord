764b622a98938126950f3dafeb22bfacf0efb890
Daniel Hicks <daniel@nup.onl>
2018-06-16 18:25:42 -0400

    Added global helper function for getting the current working directory of the user (#13)

---
+++ src/globals.cpp
@@ -0,0 +1,10 @@
+#include "globals.h"
+#include <stdio.h>
+#include <unistd.h>
+#define GetCurrentDir getcwd
+
+string global::getCurrentWorkingDirectory() {
+  char buffer[FILENAME_MAX];
+  GetCurrentDir(buffer, FILENAME_MAX);
+  return string(buffer);
+}
