5cb902ab0e65beba6b143e5ac4cec94fcc978cda
DAYFA1R <daniel@nup.onl>
2018-06-16 18:00:49 -0400

    Added command delegation to App, modified mock yaml config

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,4 @@
 #include "app.h"
-
 #include <iostream>
 #include "yaml-cpp/yaml.h"
 
@@ -16,15 +15,49 @@ App::App(int totalArgs, char** argValues) {
     // Default to help command if no args passed
     primaryCommand = "help";
   }
-  
-  // Read from master file in home directory
 }
 
 void App::init() {
-  // Testing init || TODO: remove this later
-  cout << "Active Command: " << this->primaryCommand << "\n";
+  this->delegateCommand();
 }
 
 string App::getPrimaryCommand() {
   return this->primaryCommand;
+}
+
+void App::delegateCommand() {
+  if (this->primaryCommand == "help" || this->primaryCommand == "h") {
+    cout << "help" << endl;
+    return;
+  }
+
+  // List
+  if (this->primaryCommand == "ls" || this->primaryCommand == "l") {
+    cout << "list" << endl;
+    return;
+  }
+
+  // Initialize project
+  if (this->primaryCommand == "init" || this->primaryCommand == "i") {
+    cout << "init" << endl;
+    return;
+  }
+
+  // Build
+  if (this->primaryCommand == "build" || this->primaryCommand == "b") {
+    cout << "build" << endl;
+    return;
+  }
+
+  // Run
+  if (this->primaryCommand == "run" || this->primaryCommand == "r") {
+    cout << "run" << endl;
+    return;
+  }
+
+  // Edit
+  if (this->primaryCommand == "edit" || this->primaryCommand == "e") {
+    cout << "edit" << endl;
+    return;
+  }
 }
